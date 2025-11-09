3d03d6fffc2ab2025ff72e7af69619cad15ff831
AnARCHIS12 <fakachmpionship@gmail.com>
2025-11-09 16:31:21 +0100

    Fix: Limitation reconnexions Socket.IO

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -133,7 +133,10 @@ function App() {
       transports: ['websocket', 'polling'],
       forceNew: true,
       reconnection: true,
-      timeout: 20000
+      reconnectionAttempts: 5,
+      reconnectionDelay: 2000,
+      timeout: 20000,
+      autoConnect: true
     });
     setSocket(newSocket);
 
